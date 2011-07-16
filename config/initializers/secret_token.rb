41a5879b6f8ae4b42d772d24a701cf96f83d7bd9
Takeshi Sato <master.extelligence@gmail.com>
2011-07-17 00:22:55 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kanban::Application.config.secret_token = 'da091a44f534b9af36c01e5186a6052b1a1a9f9837b57741d40e8642bb1a7ae19473e5509d224df28bddda02b40548c124f0f794bb3f9c1dbb13fa82684327ef'
