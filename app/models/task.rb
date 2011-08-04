623ecdab8716d6cd4bcaec714d35670111eb8a8e
Takeshi Sato <master.extelligence@gmail.com>
2011-08-05 01:30:13 +0900

    null回避のバリデーションを追加。

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,19 @@
 class Task < ActiveRecord::Base
+  validates :content,  :presence => true
+  validates :status,   :presence => true
+  validates :priority, :presence => true
+  validates :owner,    :presence => true
 end
+
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :integer         not null, primary key
+#  content    :text
+#  status     :string(255)
+#  priority   :integer
+#  owner      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
